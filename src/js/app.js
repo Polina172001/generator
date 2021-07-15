a83a0d645dfa3b411cd032208d039b227551eca6
Polina Filippova <polina17012001@mail.ru>
2021-07-15 22:33:46 +0300

    generator

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+import {
+  Character,
+
+  Team,
+} from './person';
+
+const person1 = new Character('Polina', 'Bowman');
+const person2 = new Character('Vika', 'Magician');
+const person3 = new Character('Kate', 'Zombie');
+
+window.teams = new Team([person1, person2, person3]);
